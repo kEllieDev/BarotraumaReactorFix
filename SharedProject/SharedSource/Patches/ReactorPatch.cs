7082725fbd3d38ba8d4594617cabee9e0424a39f
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-25 12:10:50 +0300

    Add ReactorPatch

--- SharedProject/SharedSource/Patches/ReactorPatch.cs
+++ SharedProject/SharedSource/Patches/ReactorPatch.cs
@@ -1,14 +1,27 @@
+using Barotrauma;
 using Barotrauma.Items.Components;
 using HarmonyLib;
 
 namespace ReactorFix.Patches;
 
-[HarmonyPatch( typeof(Reactor), nameof(Reactor.Update) )]
+[HarmonyPatch(typeof(Reactor), nameof(Reactor.Update))]
 public static class ReactorPatch
 {
-    public static bool Prefix( Reactor __instance )
+    public static void Postfix(Reactor __instance)
     {
-        // TODO: Apply Fix
-        return true; // Return true to continue with the original method, false to skip it
+        // Only override when wires are actively controlling it
+        if (__instance.signalControlledTargetFissionRate.HasValue &&
+            __instance.lastReceivedFissionRateSignalTime > Timing.TotalTime - 1.0)
+        {
+            __instance.TargetFissionRate = __instance.signalControlledTargetFissionRate.Value;
+            __instance.FissionRate = __instance.TargetFissionRate;
+        }
+
+        if (__instance.signalControlledTargetTurbineOutput.HasValue &&
+            __instance.lastReceivedTurbineOutputSignalTime > Timing.TotalTime - 1.0)
+        {
+            __instance.TargetTurbineOutput = __instance.signalControlledTargetTurbineOutput.Value;
+            __instance.TurbineOutput = __instance.TargetTurbineOutput;
+        }
     }
 }
