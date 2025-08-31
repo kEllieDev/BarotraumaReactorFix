83e891743cf804a9dd9361a6a1ef7de6396e7dc1
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-31 22:23:25 +0300

    Update log functions

--- SharedProject/SharedSource/Log.cs
+++ SharedProject/SharedSource/Log.cs
@@ -2,6 +2,7 @@ using Barotrauma;
 using Barotrauma.Networking;
 using Microsoft.Xna.Framework;
 using HarmonyLib;
+using System.Reflection;
 
 namespace ReactorFix;
 
@@ -10,22 +11,21 @@ namespace ReactorFix;
 /// </summary>
 public static class Log
 {
-    private static readonly string LogPath =
-        Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "/LocalMods/ReactorFix/ReactorFix_log.txt");
+    private static readonly string LogPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "/LocalMods/ReactorFix/ReactorFix_log.txt");
 
-    public static void Info(string message)
+    public static void Info(string message, ServerLog.MessageType messageType = ServerLog.MessageType.ServerMessage)
     {
-        ConsoleLog(message, Color.White);
+        ConsoleLog(message, Color.White, messageType);
     }
 
-    public static void Warning(string message)
+    public static void Warning(string message, ServerLog.MessageType messageType = ServerLog.MessageType.ServerMessage)
     {
-        ConsoleLog(message, Color.Yellow);
+        ConsoleLog(message, Color.Yellow, messageType);
     }
 
     public static void Error(string message)
     {
-        ConsoleLog(message, Color.Red);
+        ConsoleLog(message, Color.Red, ServerLog.MessageType.Error);
     }
 
     public static void FileLog(object message)
@@ -40,13 +40,12 @@ public static class Log
         }
     }
 
-    private static void ConsoleLog(string message, Color color = new Color())
+    private static void ConsoleLog(string message, Color color = new Color(), ServerLog.MessageType messageType = ServerLog.MessageType.ServerMessage)
     {
         try
         {
-            var method = AccessTools.Method(typeof(LuaCsLogger), "Log",
-                [typeof(string), typeof(Color?), typeof(ServerLog.MessageType)]);
-            method.Invoke(null, [$"[ReactorFix]: {message}", color, ServerLog.MessageType.ServerMessage]);
+            var method = AccessTools.Method(typeof(LuaCsLogger), "Log", [typeof(string), typeof(Color?), typeof(ServerLog.MessageType)]);
+            method.Invoke(null, [$"[{Assembly.GetExecutingAssembly().FullName}]: {message}", color, messageType]);
         }
         catch (Exception ex)
         {
